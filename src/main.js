9d3a880bb8e5dfb118150f26e2c4c8a41d13582a
Irene Martínez <marttre20@gmail.com>
2023-01-31 11:57:40 -0600

    Muestra todos los pokemones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+let rootBody = document.getElementById("root");
+function createCard(){
+    for (let i =0; i<data.pokemon.length; i++){
+        rootBody.innerHTML +=
+        `<div class= "cardPokemon">
+        <img src="${data.pokemon[i].img}" class="imgCard"></img>
+        <h5 class= "numCard">${data.pokemon[i].num}</h5>
+        <h5 class="nameCard">${data.pokemon[i].name}</h5>
+       </div>`
+    }
+}
 
-console.log(example, data);
+window.onload = createCard(data.pokemon);
