b4f7225622466147ba016e5a675dfc5c2315db58
alissasalazar <salazargamarraa@gmail.com>
2023-01-31 16:15:45 -0500

    feature style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+let rootBody= document.getElementById("root");
+function createCard(){
+  for(let i=0; i<data.pokemon.length; i++){
+    rootBody.innerHTML +=
+ `<div class="cardPokemon">
+ <img src="${data.pokemon[i].img}" class="imgCard"></img>
+ <h3 class="numCard"> ${data.pokemon[i].num}</h3>
+ <h4 class="nameCard"> ${data.pokemon[i].name}</h4>
+ </div>`
+  }
 
-console.log(example, data);
+}
+window.onload=createCard(data.pokemon);
