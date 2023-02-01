536d06b569afd72a3578b0b0a00533bd322005fd
Irene Martínez <marttre20@gmail.com>
2023-01-31 19:32:22 -0600

    Agrega un header y un footer

--- src/main.js
+++ src/main.js
@@ -1,31 +1,14 @@
 import data from './data/pokemon/pokemon.js';
-<<<<<<< HEAD
-// import data from './data/rickandmorty/rickandmorty.js';
-let rootBody= document.getElementById("root");
-function createCard(){
-  for(let i=0; i<data.pokemon.length; i++){
-    rootBody.innerHTML +=
- `<div class="cardPokemon">
- <img src="${data.pokemon[i].img}" class="imgCard"></img>
- <h3 class="numCard"> ${data.pokemon[i].num}</h3>
- <h4 class="nameCard"> ${data.pokemon[i].name}</h4>
- </div>`
-  }
-
-}
-window.onload=createCard(data.pokemon);
-=======
 let rootBody = document.getElementById("root");
 function createCard(){
     for (let i =0; i<data.pokemon.length; i++){
         rootBody.innerHTML +=
         `<div class= "cardPokemon">
         <img src="${data.pokemon[i].img}" class="imgCard"></img>
-        <h5 class= "numCard">${data.pokemon[i].num}</h5>
-        <h5 class="nameCard">${data.pokemon[i].name}</h5>
+        <h4 class= "numCard">#${data.pokemon[i].num}</h4>
+        <h4 class="nameCard">${data.pokemon[i].name.charAt(0).toUpperCase()+data.pokemon[i].name.slice(1)}</h4>
        </div>`
     }
 }
 
 window.onload = createCard(data.pokemon);
->>>>>>> main
